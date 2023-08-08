0931f531ce592cd8372c1510cb3ad22cbce46ae3
jrpespinas <jrpespinas@gmail.com>
2023-08-08 21:19:55 +0800

    feat: implement Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,10 +19,15 @@ const Hero = () => {
         <CustomButton
           title="Explore Cars"
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
-          handleClick={() => {
-            handleScroll;
-          }}
+          handleClick={handleScroll}
         />
+      </div>
+
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          <div className="hero__image-overlay" />
+        </div>
       </div>
     </div>
   );
